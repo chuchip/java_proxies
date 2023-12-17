347aa2c2cb3fd597ec2852871a0cb6c4553b772d
Jesus J. Puente <chuchip@gmail.com>
2023-12-17 11:44:02 +0100

    Initial commit

---
+++ src/main/java/com/profesorp/proxies/ProxiesApplication.java
@@ -0,0 +1,13 @@
+package com.profesorp.proxies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxiesApplication.class, args);
+	}
+
+}
