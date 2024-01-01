228e4ae2a70648baf31e38be5471a4132a2bfef8
Jesus J. Puente <chuchip@gmail.com>
2024-01-01 20:54:02 +0100

    Improving functionality

--- dumb/src/main/java/com/profesorp/dumb/DumbImpl.java
+++ dumb/src/main/java/com/profesorp/dumb/DumbImpl.java
@@ -1,16 +1,22 @@
 package com.profesorp.dumb;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class DumbImpl implements DumbInterface
 {
+    private static final Logger logger = LogManager.getLogger(DumbImpl.class);
+
     @Override
     public void sayHello()
     {
-        System.out.println("Hello");
+        logger.info("Hello");
     }
     @Override
     public int sum2Numbers(int num1,int num2)
     {
-        System.out.println("I'm summing two numbers");
+        logger.info("I'm adding the numbers {} {} ",num1,num2);
         return num1+num2;
     }
 
--- spring/src/main/java/com/profesorp/proxies/SimpleProxyApp.java
+++ spring/src/main/java/com/profesorp/proxies/SimpleProxyApp.java
@@ -1,9 +1,8 @@
 package com.profesorp.proxies;
+
 import com.profesorp.dumb.DumbImpl;
 import com.profesorp.dumb.DumbInterface;
 import lombok.extern.slf4j.Slf4j;
-
-
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -18,8 +17,8 @@ public class SimpleProxyApp {
         dumbClass.sayHello();
         log.info("Adding number {} to number {}. Result {}",1,2,  dumbClass.sum2Numbers(1,2));
         log.info("Adding number {} to number {}. Result {}",0,2,  dumbClass.sum2Numbers(0,2));
-      }
-
+        log.info("Adding number {} to number {}. Result {}",0,0,  dumbClass.sum2Numbers(0,0));
+    }
 }
 @Slf4j
 class DumbProxy implements InvocationHandler
@@ -31,6 +30,7 @@ class DumbProxy implements InvocationHandler
     }
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("\n");
         Object obj=null;
         switch (method.getName()) {
             case "sayHello":
@@ -41,7 +41,10 @@ class DumbProxy implements InvocationHandler
                 log.info("'sum2Numbers' function was invoked with numbers {}, {} ",args[0],args[1] );
                 if ((Integer) args[0]==0)
                     args[0]=10; // Change the number 0 to 10.
-                obj=method.invoke(target,args);
+                if ((Integer) args[1]==0)
+                    obj=-1;
+                else
+                    obj=method.invoke(target,args);
                 break;
             default:
                 log.info("I don't know what function is this: "+method.getName());
